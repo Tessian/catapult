47657946f68f07b5df61961b34dc944d54db3136
Nahuel <nahuel.lopez@tessian.com>
2023-05-05 14:24:59 +0100

    modofy release.new to accept having no docker image

--- catapult/release.py
+++ catapult/release.py
@@ -31,7 +31,7 @@ class Release:
     version: int
     commit: str
     version_id: str
-    image: str
+    image: Optional[str]
     timestamp: datetime
     author: str
     changelog: str
@@ -145,6 +145,7 @@ _DATETIME_MAX = pytz.utc.localize(datetime.max)
 
 def _get_bucket():
     config = utils.get_config()
+    print(config)
     if config:
         return config["release"]["s3_bucket"]
     return os.environ["CATAPULT_BUCKET_RELEASES"]
@@ -369,6 +370,7 @@ def list_releases(name, last, contains, bucket=None, utc=False, profile=None):
         "commit": "git ref to build from.",
         "version": "new version",
         "image-id": "ID of the docker image to release.",
+        "has-image": "Whether the deployment includes a docker image.",
         "image-name": "name of the image to release (default to name)",
         "dry": "prepare a release without committing it",
         "yes": "Automatic yes to prompt",
@@ -386,6 +388,7 @@ def new(
     version=None,
     dry=False,
     yes=False,
+    has_image=True,
     image_name=None,
     image_id=None,
     rollback=False,
@@ -413,7 +416,7 @@ def new(
     else:
         version = int(version)
 
-    if image_id is None:
+    if image_id is None and has_image is True:
         image_id = _get_image_id(ctx, commit_oid, name=name, image_name=image_name)
 
         if image_id is None:
